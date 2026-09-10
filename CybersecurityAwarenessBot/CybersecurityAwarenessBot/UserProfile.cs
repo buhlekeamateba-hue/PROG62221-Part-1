7e3dbd3a3606349ae390a0dc8c6fc393e5b6c574
buhlekeamateba-hue <buhlekeamateba@gmail.com>
2026-09-10 22:05:07 +0200

    Updated UserProfile.cs

--- CybersecurityAwarenessBot/CybersecurityAwarenessBot/UserProfile.cs
+++ CybersecurityAwarenessBot/CybersecurityAwarenessBot/UserProfile.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CybersecurityAwarenessBot
 {
-    internal class UserProfile
+    public class UserProfile
     {
         //stores the user's name
         public string Name { get; set; } = "";
